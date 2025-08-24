efcd73feda770e22065d4c4b81db09cbf016e0e2
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-24 17:38:31 +0530

    Checking

--- BasicOfJS/01_Basics.js
+++ BasicOfJS/01_Basics.js
@@ -6,11 +6,3 @@ console.log(a);
 var a;
 
 // WarmUp thingsss...!!
-// Agr mein var lagu ya na lagu value print hote huye dikhegi, var ko declare kre bina agr mein log krdu to wo  print hojjyga and if log k bbaad  bhi var declare krdu to bhi print hojjyga, but let and const k case mein aisa nhi hoga, wo error dega, NOW WHY THIS IS HAPPENING...???
-
-// agr aap var se  variable bnate ho to aap uske delare krne se phale use kr skte ho, khene ka mtlb phele istmal kre phir wisas kre
-a = 12;
-console.log(a);
-var a;
-
-// WarmUp thingsss...!!
