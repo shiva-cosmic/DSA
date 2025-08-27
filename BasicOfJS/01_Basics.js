4d15adeb0660dd245b22afb25d3a06cffdb3b414
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-27 21:29:49 +0530

    mm

--- BasicOfJS/01_Basics.js
+++ BasicOfJS/01_Basics.js
@@ -28,3 +28,33 @@ let a = 12;
 let b = "13";
 
 console.log(a + b);
+// Agr mein var lagu ya na lagu value print hote huye dikhegi, var ko declare kre bina agr mein log krdu to wo  print hojjyga and if log k bbaad  bhi var declare krdu to bhi print hojjyga, but let and const k case mein aisa nhi hoga, wo error dega, NOW WHY THIS IS HAPPENING...???
+
+// agr aap var se  variable bnate ho to aap uske delare krne se phale use kr skte ho, khene ka mtlb phele istmal kre phir wisas kre
+// a = 12;
+// console.log(a);
+// var a;
+
+// WarmUp thingsss...!!
+// Agr mein var lagu ya na lagu value print hote huye dikhegi, var ko declare kre bina agr mein log krdu to wo  print hojjyga and if log k bbaad  bhi var declare krdu to bhi print hojjyga, but let and const k case mein aisa nhi hoga, wo error dega, NOW WHY THIS IS HAPPENING...???
+
+// agr aap var se  variable bnate ho to aap uske delare krne se phale use kr skte ho, khene ka mtlb phele istmal kre phir wisas kre
+// a = 12;
+// console.log(a);
+// var a;
+
+// WarmUp thingsss...!!
+
+// Haluya Question
+
+// let a = 10;
+// let b = 20;
+
+// console.log(a + b);
+
+// Difference between no. and string is number is look like 10 only and string is look like "10"
+
+let a = 12;
+let b = "13";
+
+console.log(a + b);
