f97739e1d22844a727a6c2567b1bfd0c7b704613
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-28 18:27:10 +0530

    priority

---
+++ BasicOfJS/02_Basic.js
@@ -0,0 +1,4 @@
+let a = 10;
+let b = 20;
+
+console.log("sum of 10 and 20 is " + (a + b)); //Result: sum of 10 and 20 30 and it give priority to brackets  and solve that first after that it concincatinate
