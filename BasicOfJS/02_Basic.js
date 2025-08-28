d14998725e6063097b15f55f0b52779488157d49
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-28 22:16:41 +0530

    Typecurson

--- BasicOfJS/02_Basic.js
+++ BasicOfJS/02_Basic.js
@@ -1,4 +1,12 @@
 let a = 10;
 let b = 20;
 
+// case 1
 console.log("sum of 10 and 20 is " + (a + b)); //Result: sum of 10 and 20 30 and it give priority to brackets  and solve that first after that it concincatinate
+
+// case 2
+console.log(a + b + " is sum of 10 and 20");
+
+// typecurson
+console.log("1" + 1); // result: 11 concatenante krdeg
+console.log("1" - 1); // result: 0 , internpreter string ko number mein convert krdega and then us no. se subrtact kr dega.
