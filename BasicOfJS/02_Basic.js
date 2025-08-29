2ebd95ec136bc954f0d9e564647fc45f442aa160
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-29 19:08:57 +0530

    update

--- BasicOfJS/02_Basic.js
+++ BasicOfJS/02_Basic.js
@@ -10,3 +10,7 @@ console.log(a + b + " is sum of 10 and 20");
 // typecurson
 console.log("1" + 1); // result: 11 concatenante krdeg
 console.log("1" - 1); // result: 0 , internpreter string ko number mein convert krdega and then us no. se subrtact kr dega.
+
+// Q: Accept and print the answer
+let age = prompt("What is ur age:");
+console.log(typeof age);
