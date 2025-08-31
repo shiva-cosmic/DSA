dcfe8e491506586b9f3e35b87d10302bc74c613d
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-31 08:13:50 +0530

    divde and mod

---
+++ BasicOfJS/04_Operators.js
@@ -0,0 +1,10 @@
+let a = 12;
+let b = 22;
+
+console.log(Math.floor(a / b)); // if u want that ki point mein answer na aye divide krne k baad to math.floor ka use kr skte ho
+
+// division humesa qutient deta hai answer mein
+
+console.log(a % b);
+
+// MOD humesa reminder deta hai answer mein
