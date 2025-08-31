f6e00d184f4d5abe3a701653b0a8da441599ea70
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-31 12:35:31 +0530

    LogicalOp

---
+++ BasicOfJS/06_logicalOp.js
@@ -0,0 +1,12 @@
+//   && ||
+console.log(10 > 6 && 15 < 9); // && AND wale case mein dono stament true hongi tabhi he true retun hoga, otherwise ek bhi agr false huyi to false return kreg.  inshort dono true to true ek bhi false to false return krega.
+
+// Aur agr pheli condition false agyi to wo age check he nhi krega AND k case mein
+
+//  ||  0R Operator
+
+console.log(10 < 6 || 15 < 9 || 18 > 9);
+
+// In OR case if any one of the condition is true then it return true. and it give false only when all condition is false.
+
+// Aur ismein agr pheli he condition true aa jati hai to wo agee check nhi krega
