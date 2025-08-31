a78e7ec13d145f67b26f3649e6b12abe17b89aa3
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-31 11:37:01 +0530

    Relational op"

---
+++ BasicOfJS/05_relationalOperator.js
@@ -0,0 +1,11 @@
+// Relational Operators: > , < , <= , >= , !==
+
+// It shows the relation between two vaules and according to that relation it decide wheather the statement is True Or False
+
+// = and  ==  and ===
+
+// = : Assingment Operator, value assign krta hai
+
+// == : It checks the value wheather it is same or not. if yes then it retun true if no then it return false.
+
+// === : strick Check, it checks values and as well as data type and return ture false when all properties is same LHS EQUAL TO RHS
