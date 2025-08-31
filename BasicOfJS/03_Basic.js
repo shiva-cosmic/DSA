165b09fd6501c95c15b6edc6576f0bfa0bfb2a21
Shivanshh369 <shivanshhh369@gmail.com>
2025-08-31 07:53:05 +0530

    Swap

---
+++ BasicOfJS/03_Basic.js
@@ -0,0 +1,32 @@
+// Swaping
+
+// 1.Sawap With Extra Variable
+// let a = 10;
+// let b = 20;
+// let c;
+
+// c = a; //c =10 , a = 10;
+// a = b; // a = 20 , b = 20
+// b = c; // b = 10
+
+// console.log(a);
+// console.log(b);
+
+//2.Sawap Without Extra Variable
+// let a = 10;
+// let b = 20;
+
+// a = a + b; // a = 30
+// b = a - b; // b = 10
+// a = a - b;
+
+console.log(a, b);
+
+// 3. Swap
+
+let a = 10;
+let b = 20;
+
+[a, b] = [b, a];
+
+console.log(a, b);
