014708ca8fda6e9f0e51253bdba882baaf2d57e4
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-03 12:08:02 +0530

    added

--- Conditionals/01_Conditional.js
+++ Conditionals/01_Conditional.js
@@ -2,7 +2,9 @@
 
 let ans = Number(prompt("what is your age?"));
 
-if (ans >= 18) {
+if (isNaN(ans)) {
+  console.log("Wrong Input");
+} else if (ans >= 18) {
   console.log("You can vote");
 } else {
   console.log("You can not vote");
