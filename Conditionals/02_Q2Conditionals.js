9f1fad4cb99fd8d9df85aef3dbb2422b9a03e207
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-05 11:48:36 +0530

    Efficent Way

--- Conditionals/02_Q2Conditionals.js
+++ Conditionals/02_Q2Conditionals.js
@@ -1,15 +1,18 @@
 // Q2: Shop Discount
 
 let amount = Number(prompt("What is the final amount ?"));
+let dis = 0;
 
 if (amount > 0 && amount <= 5000) {
-  console.log(amount);
+  dis = 0;
 } else if (amount > 5000 && amount <= 7000) {
-  console.log(amount - Math.floor((5 * amount) / 100));
+  dis = 5;
 } else if (amount > 7000 && amount <= 9000) {
-  console.log(amount - Math.floor((10 * amount) / 100));
+  dis = 10;
 } else if (amount > 9000) {
-  console.log(amount - Math.floor((20 * amount) / 100));
-} else {
+  dis = 20;
+} else if (isNaN(amount)) {
   console.log("Wrong Input");
 }
+
+console.log(amount - Math.floor(dis * amount) / 100);
