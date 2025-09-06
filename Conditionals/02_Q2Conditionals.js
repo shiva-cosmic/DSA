1b0b324ee6c603c46aee967f57a7d4d21212457f
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-06 14:01:45 +0530

    fixed

--- Conditionals/02_Q2Conditionals.js
+++ Conditionals/02_Q2Conditionals.js
@@ -16,21 +16,3 @@ if (amount > 0 && amount <= 5000) {
 }
 
 console.log(amount - Math.floor(dis * amount) / 100);
-// Q2: Shop Discount
-
-let amount = Number(prompt("What is the final amount ?"));
-let dis = 0;
-
-if (amount > 0 && amount <= 5000) {
-  dis = 0;
-} else if (amount > 5000 && amount <= 7000) {
-  dis = 5;
-} else if (amount > 7000 && amount <= 9000) {
-  dis = 10;
-} else if (amount > 9000) {
-  dis = 20;
-} else if (isNaN(amount)) {
-  console.log("Wrong Input");
-}
-
-console.log(amount - Math.floor(dis * amount) / 100);
