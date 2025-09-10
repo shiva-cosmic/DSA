f87938877eee40ba84206715a5e9312c145f128d
Shivanshh369 <shivanshhh369@gmail.com>
2025-09-10 23:17:00 +0530

    check

--- Conditionals/03_ElectricityBill.js
+++ Conditionals/03_ElectricityBill.js
@@ -15,3 +15,20 @@ if (unit > 100 && unit <= 200) {
 }
 amount += unit * 4;
 console.log(amount);
+// Q10: Electricity Bill
+let unit = Number(prompt("Enter Electricity unit")); //700
+let amount = 0;
+if (unit > 400) {
+  amount = (unit - 400) * 13; //3900
+  unit = 400;
+} //400
+if (unit > 200 && unit <= 400) {
+  amount = (unit - 200) * 8; //1600
+  unit = 200;
+} //200
+if (unit > 100 && unit <= 200) {
+  amount += (unit - 100) * 6; //600
+  unit = 100;
+}
+amount += unit * 4;
+console.log(amount);
